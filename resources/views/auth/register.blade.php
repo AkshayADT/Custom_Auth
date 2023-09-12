95724386db51c3ee3cd9c38536bec1935b9d805e
AkshayADT <akshay.dangi@engineermaster.in>
2023-09-12 13:10:53 +0530

    view model table

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+</head>
+
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <h4>Registration</h4>
+                <form action="">
+                    <div class="form-group">
+                        <label for="">name</label>
+                        <input class="form-control" placeholder="Enter Your Name" type="text" name="name"
+                            value="">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input class="form-control" placeholder="Enter Your email" type="email" name="email"
+                            value="">
+                    </div>
+                    <div class="form-group">
+                        <label for="">password</label>
+                        <input class="form-control" placeholder="Enter Your password" type="password" name="password"
+                            value="">
+                    </div>
+                   <div class="form-group pt-3">
+                    <button type="submit" class="btn btn-block btn-dark">Submit</button>
+                   </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [CustomAuthController::class, 'login'])->name('login');
+Route::get('/register', [CustomAuthController::class, 'register'])->name('register');
